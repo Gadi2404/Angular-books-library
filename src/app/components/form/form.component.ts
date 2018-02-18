0bfd043f7e0932e8add8952b7cd54ea0a1d727e8
Gadi2404 <gadi2404@gmail.com>
2018-02-19 00:39:32 +0200

    added option to add image link in form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -9,6 +9,7 @@ export class FormComponent implements OnInit {
     @Input() formData;
     @Output() onFormUpdate: EventEmitter<any> = new EventEmitter<any>();
     model;
+    defaultImg = 'assets/images/imageNotAvailable.jpg';
 
     constructor() {}
 
@@ -29,8 +30,13 @@ export class FormComponent implements OnInit {
         for( let prop in this.model ){
             this.model[prop] = this.model[prop].trim();
 
-            if( prop !== 'id' && prop !== 'image' && !this.model[prop] ) return
+            if( prop !== 'id' && !this.model[prop] )
+                return
         }
+
+        // set a default image if image is not loading
+        if( !this.isImageLoading(this.model.image) )
+            this.model.image = this.defaultImg;
 
         this.onFormUpdate.emit({
             actionType: 'submit',
@@ -50,9 +56,24 @@ export class FormComponent implements OnInit {
                             new Book(this.formData.title,
                                      this.formData.author,
                                      this.formData.date,
-                                     this.formData.image,
+                                     this.getBookImageValue(this.formData.image),
                                      this.formData.id) :
                             new Book()
+    }
+
+    // display images that are not the default (not available image)
+    getBookImageValue(currentImg){
+        return currentImg === this.defaultImg? '' : currentImg
+    }
+
+    isImageLoading(imgSrc: string){
+        let imgObj = new Image();
+        imgObj.src = imgSrc;
+
+        if( !imgObj.complete )
+            return false
+
+        return true
     }
 
 }
@@ -62,7 +83,7 @@ class Book {
     public title: string = '',
     public author: string = '',
     public date: string = '',
-    public image: string = 'https://islandpress.org/sites/default/files/400px%20x%20600px-r01BookNotPictured.jpg',
+    public image: string = '',
     public id: string = ''
   ) {  }
 }
